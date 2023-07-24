6ac8f983f72a964d5e6b90efaa5aac6e0b4a3b78
563596131 <wang.yidan2@northeastern.edu>
2023-07-24 16:35:44 -0400

    Update 239.sliding-window-maximum.java

--- Slide Window/239.sliding-window-maximum.java
+++ Slide Window/239.sliding-window-maximum.java
@@ -5,9 +5,32 @@
  */
 
 // @lc code=start
+
+import java.util.Deque;
+import java.util.LinkedList;
+
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        int len = nums.length;
+        if(len==0 || k==0) return new int[0];
+        int[] res = new int[len-k+1];
+        Deque<Integer> d = new LinkedList<>();
 
+        for(int i=0; i<len; i++) {
+            //remove the indices that are out of bound
+            while(d.size()>0 && d.peekFirst()<=i-k) {
+                d.pollFirst();
+            }
+            //remove indices whose corresponding value is less than nums[i]
+            
+            while(d.size()>0 && nums[d.peekLast()]<=nums[i]) {
+                d.pollLast();
+            }
+
+            d.offerLast(i);
+            if(i-k+1>=0) res[i-k+1] = nums[d.peekFirst()];
+        }
+        return res;
     }
 }
 // @lc code=end
