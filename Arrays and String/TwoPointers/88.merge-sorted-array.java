67d766a4e9a07360c3dc22436e272934b2f0fde1
563596131 <wang.yidan2@northeastern.edu>
2023-07-27 02:23:05 -0400

    7.27 leedcode interview

---
+++ Arrays and String/TwoPointers/88.merge-sorted-array.java
@@ -0,0 +1,26 @@
+package TwoPointers;
+/*
+ * @lc app=leetcode id=88 lang=java
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m-1, p2 = n-1, p3 = m+n-1;
+
+        while(p2>=0) {
+            if(p1>=0 && nums1[p1]>nums2[p2]) {
+                nums1[p3] = nums1[p1];
+                p1--;
+            }else {
+                nums1[p3] = nums2[p2];
+                p2--;
+            }
+            p3--;
+        }
+    }
+}
+// @lc code=end
+
