53b6006d3647956f2138491275cf8c9c50b16295
563596131 <wang.yidan2@northeastern.edu>
2023-07-26 20:31:16 -0400

    Create 38.count-and-say.java

---
+++ Arrays and String/38.count-and-say.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=38 lang=java
+ *
+ * [38] Count and Say
+ */
+
+// @lc code=start
+
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+    public String countAndSay(int n) {
+        String s = "1";
+
+        for(int i=1; i<n; i++) {
+            s = helper(s);
+        }
+        return s;
+    }
+    
+    public String helper(String s) {
+        //s = "111221" ->312211
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        //int end = 0;
+        for(int i=0; i<s.length(); i++) {
+            while(i<s.length()-1 && s.charAt(i)==s.charAt(i+1)) {
+                count++;
+                i++;
+            }
+            sb.append(count);
+            sb.append(s.charAt(i));
+            count = 1;
+
+        }
+
+        return sb.toString();
+
+
+    }
+}
+// @lc code=end
+
