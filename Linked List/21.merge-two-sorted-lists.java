d30245d15024c6b9f559ebd16b0d053d7e775cd3
563596131 <wang.yidan2@northeastern.edu>
2023-07-27 15:55:54 -0400

    Create 21.merge-two-sorted-lists.java

---
+++ Linked List/21.merge-two-sorted-lists.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=21 lang=java
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //dummy node
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+       // ListNode p1 = list1, p2 = list2;
+
+        while(list1!=null && list2!=null) {
+            if(list1.val<list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            }else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        if(list1!=null) cur.next = list1;
+        if(list2!=null) cur.next = list2;
+        return dummy.next;
+    }
+}
+// @lc code=end
+
