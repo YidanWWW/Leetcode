17d4a894fec4c063ca7fabaed3d70bc59864dddc
563596131 <wang.yidan2@northeastern.edu>
2023-07-27 14:46:37 -0400

    Linked List

--- Linked List/160.intersection-of-two-linked-lists.java
+++ Linked List/160.intersection-of-two-linked-lists.java
@@ -18,37 +18,17 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA==null || headB==null) return null;
-        int len1 = 0;
-        ListNode p1 = headA;
-        while(p1!=null) {
-            p1 = p1.next;
-            len1++;
-        }
-        int len2 = 0;
-        ListNode p2 = headB;
-        while(p2!=null) {
-            p2 = p2.next;
-            len2++;
-        }
-        int len = Math.abs(len1-len2);
-        while(len>=0) {
-            if(len1>0) {
-                headA = headA.next;
-            }else {
-                headB = headB.next;
-            }
-            len--;
-        }
-        while(headA!=null || headB!=null) {
-            if(headA==headB){
-                return new ListNode(headA.val);
-            }else {
-                headA = headA.next;
-                headB = headB.next;
-            }
-        }
-        return null;
+        if(headA == null || headB == null) return null;
+
+        ListNode p1=headA;
+        ListNode p2=headB;
+
+        while(p1!=p2) {
+            p1 = p1==null?headB:p1.next;
+            p2 = p2==null?headA:p2.next;
+        } 
+        return p1;
+    
     }
 }
 // @lc code=end
