16f500a41e6fa78916f2778b515dd857db17f7bf
563596131 <wang.yidan2@northeastern.edu>
2023-07-27 21:46:27 -0400

    Create 26.remove-duplicates-from-sorted-array.java

---
+++ Arrays and String/TwoPointers/26.remove-duplicates-from-sorted-array.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=26 lang=java
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        //two pointers:i,j
+        //j: keep track of the current index where a unique element shoule be p;aced
+        int i=1, j=1;
+        for(;i<nums.length; i++) {
+            if(nums[i] != nums[i-1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
+// @lc code=end
+
