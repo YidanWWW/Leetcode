c133a2b6f303a1a2e01bf7c49b8288bdd91b549b
563596131 <wang.yidan2@northeastern.edu>
2023-08-01 16:03:43 -0400

    Create 2401.longest-nice-subarray.java

---
+++ Arrays and String/Slide Window/2401.longest-nice-subarray.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=2401 lang=java
+ *
+ * [2401] Longest Nice Subarray
+ */
+
+// @lc code=start
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int start = 0, end = 0, AND = 0, res = 1;
+        while(end<nums.length){
+            while((AND & nums[end]) > 0) {
+                AND ^= nums[start];
+                start++;
+            }
+            AND |= nums[end];
+            res = Math.max(res, end-start+1);
+            end++;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
