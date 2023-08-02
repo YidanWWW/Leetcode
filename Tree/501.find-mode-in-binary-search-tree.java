5238835049770bf8e38d99783c763582e772264d
563596131 <wang.yidan2@northeastern.edu>
2023-08-02 18:26:31 -0400

    Update 501.find-mode-in-binary-search-tree.java

--- Tree/501.find-mode-in-binary-search-tree.java
+++ Tree/501.find-mode-in-binary-search-tree.java
@@ -21,8 +21,36 @@
  * }
  */
 class Solution {
-    public int[] findMode(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        //level traversal
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int count = 1; //count is even, from left to right, if odd,reverse order
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null) return res;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0; i<size; i++) {
+                TreeNode node = q.poll();
+                list.add(node.val);
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
+            }
         
+            if(count%2==0) list = reverse(list);
+            res.add(list);
+            count++;
+        }
+        return res;
+    }
+
+    public List<Integer> reverse(List<Integer> list) {
+        List<Integer> res = new ArrayList<>();
+        for(int i=list.size()-1; i>=0; i--) {
+            res.add(list.get(i));
+        }
+        return res;
     }
 }
 // @lc code=end
