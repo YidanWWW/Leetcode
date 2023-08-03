28242aece52488160e35a64a379e51af683a679d
563596131 <wang.yidan2@northeastern.edu>
2023-08-03 18:30:21 -0400

    Update 200.number-of-islands.java

--- DFS/200.number-of-islands.java
+++ DFS/200.number-of-islands.java
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution1 {
     int[][] dirs = {{1,0}, {-1,0},{0,1},{0,-1}};
     public int numIslands(char[][] grid) {
         //1.DFS
@@ -32,5 +32,48 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    int[] root = null;
+    int count;
+    public int numIslands(char[][] grid) {
+        //2. union find
+        int m = grid.length, n = grid[0].length;
+        int count = m*n;
+        root = new int[count];
+
+        for(int i=0; i<count; i++) root[i] = i;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                //2d->1d
+                if(grid[i][j]=='1') {
+                    if(i>0 && grid[i-1][j]=='1') merge(i*n+j, (i-1)*n+j);
+                    if(j>0 && grid[i][j-1]=='1') merge(i*n+j, i*n+j-1);
+                }else {
+                    count--;
+                }
+            }
+        }
+        return count;
+
+    }
+
+    public int findRoot(int x) {
+        if(x==root[x]) return x;
+        root[x] = findRoot(root[x]);
+        return root[x];
+    }
+
+    public void merge(int x, int y) {
+        x = findRoot(x);
+        y = findRoot(y);
+        if(x!=y) {
+            root[x] = y;
+            count--;
+        }
+    }
+}
+
+
 // @lc code=end
 
