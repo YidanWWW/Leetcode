b71ed56072caa8553c4a7b1a47b686a0a23e59d9
563596131 <wang.yidan2@northeastern.edu>
2023-08-07 22:51:48 -0400

    Update 981.time-based-key-value-store.java

--- Binary search/981.time-based-key-value-store.java
+++ Binary search/981.time-based-key-value-store.java
@@ -10,18 +10,37 @@ import java.util.HashMap;
 import java.util.List;
 
 class TimeMap {
-    HashMap<String, List<Pair<String, Integer>>> map;
-
+    HashMap<String, List<Pair<String,Integer>>> map;
     public TimeMap() {
         this.map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        if(map.containsKey())
+        if(!map.containsKey(key)) {
+            map.put(key, new ArrayList<>());
+        }
+        map.get(key).add(new Pair<>(value, timestamp));
     }
     
     public String get(String key, int timestamp) {
+        if(!map.containsKey(key)) return "";
         
+        int res = search(map.get(key), timestamp);
+        if(res==-1) return "";
+        return map.get(key).get(res).getKey();
+    }
+    public int search(List<Pair<String,Integer>> list, int target) {
+        
+        int left = -1, right = list.size(); //<=target
+        while(left+1!=right) {
+            int mid = left+(right-left)/2;
+            if(list.get(mid).getValue()<=target) {
+                left = mid;
+            }else {
+                right = mid;
+            }
+        }
+        return left;
     }
 }
 
